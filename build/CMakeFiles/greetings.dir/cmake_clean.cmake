c0b09151aea6c32701d71b349d96524424da74af
Sammy <samuelhooper888@gmail.com>
2025-04-10 15:11:04 +0200

    Made directory for ci.yml and created the yaml

---
+++ build/CMakeFiles/greetings.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/user_wsl2/git-docker-tutorial/docker-exercise/run/greetings"
+  "/home/user_wsl2/git-docker-tutorial/docker-exercise/run/greetings.pdb"
+  "CMakeFiles/greetings.dir/src/greetings.cpp.o"
+  "CMakeFiles/greetings.dir/src/greetings.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greetings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/greetings.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for greetings.
+# This may be replaced when dependencies are built.
